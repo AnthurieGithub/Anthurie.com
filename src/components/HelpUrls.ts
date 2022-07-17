02818f60ba8f6aa1a84cc8d8931c8a7f4e9c61a8
7-- <regop412@gmail.com>
2022-07-16 21:21:37 -0400

    remove percents on slider. change footer URLs

--- src/components/HelpUrls.ts
+++ src/components/HelpUrls.ts
@@ -1,12 +1,5 @@
 export const helpUrls = {
-  customerSupport: 'https://t.me/ProjectSerum',
-  customerSupportZh: 'https://t.me/ProjectSerum_Chinese',
-  contactEmail: 'mailto:contact@projectserum.com',
-  discord: 'https://discord.gg/raydium',
-  telegram: 'https://t.me/raydiumprotocol',
-  github: 'https://github.com/raydium-io/raydium-dex',
-  twitter: 'https://twitter.com/RaydiumProtocol',
-  raydium: 'https://raydium.io/',
-  developerResources: 'https://projectserum.com/developer-resources',
+  discord: 'https://discord.gg/XCqdBHNP',
+  twitter: 'https://twitter.com/philrego',
   solanaBeach: 'https://solanabeach.io',
 };
--- src/components/TradeForm.tsx
+++ src/components/TradeForm.tsx
@@ -30,11 +30,11 @@ const BuyButton = styled(Button)`
 `;
 
 const sliderMarks = {
-  0: '0%',
+ /*  0: '0%',
   25: '25%',
   50: '50%',
   75: '75%',
-  100: '100%',
+  100: '100%', */
 };
 
 export default function TradeForm({
