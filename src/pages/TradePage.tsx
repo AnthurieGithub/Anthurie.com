8fc871f9921af355d97b02bcadb70a0f970de45f
7-- <regop412@gmail.com>
2022-07-17 13:01:23 -0400

    referal fees address. change Title

--- src/pages/TradePage.tsx
+++ src/pages/TradePage.tsx
@@ -83,7 +83,7 @@ function TradePageInner() {
   });
 
   useEffect(() => {
-    document.title = marketName ? `${marketName} — Raydium` : 'Raydium';
+    document.title = marketName ? `${marketName} ` : 'Anthurie';
   }, [marketName]);
 
   const changeOrderRef = useRef<
